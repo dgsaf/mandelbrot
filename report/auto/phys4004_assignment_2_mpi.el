ad7287bd8879cde342e6f7bfd18f615cdd298ba8
Tom Ross <tom.ross@protonmail.com>
2021-05-06 23:49:55 +0800

    Included load-balancing figures for extreme chunk-size cases.

--- report/auto/phys4004_assignment_2_mpi.el
+++ report/auto/phys4004_assignment_2_mpi.el
@@ -1,6 +1,9 @@
 (TeX-add-style-hook
  "phys4004_assignment_2_mpi"
  (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -15,14 +18,18 @@
     "sec:static"
     "fig:static-load-balance"
     "sec:master-worker"
-    "fig:master-load-balance"
+    "fig:master-load-balance-100000"
     "fig:scaling-chunksize"
+    "fig:master-load-balance-10"
+    "fig:master-load-balance-10000000"
     "sec:cyclic"
     "fig:cyclic-load-balance"
     "sec:appendix"
     "sec:serial-code"
     "sec:static-code"
     "sec:master-worker-code"
-    "sec:cyclic-code"))
+    "sec:cyclic-code")
+   (LaTeX-add-listings-lstdefinestyles
+    "ff"))
  :latex)
 
